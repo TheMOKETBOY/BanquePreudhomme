1c9b0394ae67da76989691165f190dcaba1220e8
TheMOKETBOY <tatelvilrus.samuel@gmail.com>
2021-03-19 16:00:53 +0100

    Initialisation du Projet

---
+++ src/main/java/com/example/preudhomme/PreudhommeApplication.java
@@ -0,0 +1,14 @@
+package com.example.preudhomme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreudhommeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreudhommeApplication.class, args);
+
+    }
+
+}
